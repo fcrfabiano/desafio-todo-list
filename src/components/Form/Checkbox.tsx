df7e90ab39a2585c29994fa15423784ac365154d
Fabiano César <fcfabiano.cesar@gmail.com>
2024-02-18 15:32:15 -0300

    corrije o tamanho do checkbox

--- src/components/Form/Checkbox.tsx
+++ src/components/Form/Checkbox.tsx
@@ -5,7 +5,11 @@ import styles from "./styles.module.css";
 type CheckboxProps = {} & InputHTMLAttributes<HTMLInputElement>;
 
 const Checkbox: FC<CheckboxProps> = ({ ...props }) => {
-  return <input className={styles.checkbox} type="checkbox" {...props} />;
+  return (
+    <div className={styles.checkboxWrapper}>
+      <input className={styles.checkbox} type='checkbox' {...props} />
+    </div>
+  );
 };
 
 export default Checkbox;
