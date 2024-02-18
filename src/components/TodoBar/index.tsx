d11898dc60fbe12c065fbbf00f611e42a07e77ba
Fabiano César <fcfabiano.cesar@gmail.com>
2024-02-18 14:49:37 -0300

    cria todo bar

---
+++ src/components/TodoBar/index.tsx
@@ -0,0 +1,58 @@
+import { FC } from "react";
+
+import styles from "./styles.module.css";
+import { Form } from "../Form";
+import { PlusCircle } from "@phosphor-icons/react";
+import { FormProvider, useFieldArray, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { todo } from "../../lib/schemas/validation";
+import { z } from "zod";
+
+type Todo = z.infer<typeof todo>;
+
+const TodoBar: FC = ({ ...props }) => {
+  const createTodoList = useForm<Todo & { todos: any[] }>({
+    criteriaMode: "all",
+    defaultValues: {
+      todo: "",
+      todos: [],
+    },
+    mode: "all",
+    resolver: zodResolver(todo),
+  });
+
+  const { handleSubmit, control } = createTodoList;
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "todos",
+  });
+
+  const handleCreateTodo = (newTodo: Todo) => {
+    console.log("====================================");
+    console.log(newTodo);
+    console.log("====================================");
+  };
+
+  return (
+    <FormProvider {...createTodoList}>
+      <form
+        onSubmit={handleSubmit(handleCreateTodo)}
+        className={styles.todoBar}
+      >
+        <Form.Field>
+          <Form.Input name='todo' placeholder='Adicione uma nova tarefa' />
+          <Form.ErrorMessage field='todo' />
+        </Form.Field>
+        <Form.Button
+          type='submit'
+          endIcon={<PlusCircle size={20} weight='bold' />}
+        >
+          Criar
+        </Form.Button>
+      </form>
+    </FormProvider>
+  );
+};
+
+export default TodoBar;
